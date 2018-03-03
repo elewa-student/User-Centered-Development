66e44bac6910b7d73b78c5168d5be3e2ac8e58df
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-03-03 16:27:14 +0100

    gremlins!

---
+++ tests/release.js
@@ -0,0 +1,5 @@
+  gremlins           // Yo, Gremlins
+    .createHorde()   // Create me a default horde
+    .unleash();      // Then immediately release them
+
+// https://codepen.io/aliciasedlock/pen/wowrmG
